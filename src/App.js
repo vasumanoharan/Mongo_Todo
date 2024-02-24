acf440cd71750491758a6939e58e0fc835e37aa4
vasumanoharan <vasumanoharan98@gmail.com>
2024-02-24 10:51:02 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/todos')
+      .then(response => setTodos(response.data))
+      .catch(error => console.error('Error fetching todos:', error));
+  }, []);
+
+  const addTodo = () => {
+    axios.post('http://localhost:5000/todos', { task: newTask })
+      .then(response => {
+        setTodos([...todos, response.data]);
+        setNewTask('');
+      })
+      .catch(error => console.error('Error adding todo:', error));
+  };
+
+  const deleteTodo = (id) => {
+    axios.delete(`http://localhost:5000/todos/${id}`)
+      .then(() => {
+        const updatedTodos = todos.filter(todo => todo._id !== id);
+        setTodos(updatedTodos);
+      })
+      .catch(error => console.error('Error deleting todo:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.task}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Todo;
